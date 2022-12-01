73fab68034d76537538cbf668a9a6987d7f0abc8
NadyaTambunan <nadiaputri99tambunan@gmail.com>
2022-12-01 14:31:38 +0700

    tugas7.js

---
+++ tugas7.js
@@ -0,0 +1,10 @@
+var tinggi = [189, 156, 140, 130, 180, 
+                170, 188, 165, 143, 156];
+
+for(let x of tinggi){
+    console.log(x)
+}
+console.log("")
+for(var i=0; i<tinggi.length; i++){
+    console.log(tinggi[i])
+}
